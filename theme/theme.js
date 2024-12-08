b4c12bf4d194912d47146db3208ac11c7bda7439
Bryan Baboolal <156617638+bryanbab@users.noreply.github.com>
2024-12-08 18:47:33 -0500

    Add files via upload

---
+++ theme/theme.js
@@ -0,0 +1,24 @@
+import { background } from "@cloudinary/url-gen/qualifiers/focusOn";
+
+const theme = {
+    light: {
+        theme: 'light',
+        color: 'black',
+        background: 'white',
+        backgroundColor: '#fff',
+        shadowColor: '#D3D3D3',
+        buttonColor: '#fff',
+        buttonBorderColor: '#d3d3d3'
+    },
+    dark: {
+        theme: 'dark',
+        color: 'white',
+        background: 'black',
+        backgroundColor: '#212124',
+        buttonColor: '#818181',
+        shadowColor: '#161618',
+        buttonBorderColor:'#818181',
+
+    }
+}
+export default theme;
